9ca7d80b5ce0ff2af44e8d0e9d4f092df7bbe56d
defpunk aka David Oliver <david.oliver@qwickr.com>
2015-11-25 06:51:15 +0000

    Added basic test of uuid to encoded string.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uuid-encoder "0.1.0-SNAPSHOT"
+(defproject com.qwickr/uuid-encoder "0.1.0-SNAPSHOT"
   :description "Utiliities for encoding UUIDs. Version one will support base58 encoding"
   :url "https://github.com/defpunk/uuid-encoder"
   :license {:name "Eclipse Public License"
@@ -16,9 +16,8 @@
 
 
 
-  :deploy-repositories [["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-                                   :creds :gpg}
-                       "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
-                                    :creds :gpg}]]
+  :deploy-repositories [
+        ["releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/" :creds :gpg}]
+        ["snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/" :creds :gpg}]]
 
   )
--- test/com/qwickr/test/uuid.clj
+++ test/com/qwickr/test/uuid.clj
@@ -1,1 +1,13 @@
-uuid.clj
+(ns com.qwickr.test.uuid
+  (:require [clojure.test :refer :all]
+  			[com.qwickr.uuid :refer :all])
+)
+
+
+(deftest test-encoded-longs
+	"test encoding works for known values of uuids"
+  (is (= "1111111111111111" (base58-encode-uuid (java.util.UUID. 0 0))))
+  (is (= "1111111111111112" (base58-encode-uuid (java.util.UUID. 0 1))))
+  (is (= "111111111111111z" (base58-encode-uuid (java.util.UUID. 0 57))))
+  (is (= "11111111111111121" (base58-encode-uuid (java.util.UUID. 0 58))))
+  )
